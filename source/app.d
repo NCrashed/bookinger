cf6b0794da382bdb2d9c4bca7f1accdc0d2468de
NCrashed <NCrashed@gmail.com>
2014-03-26 02:20:25 +0400

    Simple frontend

--- source/app.d
+++ source/app.d
@@ -1,16 +1,27 @@
 import vibe.d;
 
-void handleRequest(HTTPServerRequest req,
-                   HTTPServerResponse res)
+struct Question
 {
-    res.writeBody("Hello, World!", "text/plain");
+    string text = "Are you afraid of death, Mr. Freeman?";
+    string[] answers = ["No, die octopus!", "Yes, i shall serve you!"];
+}
+
+void index(HTTPServerRequest req,
+           HTTPServerResponse res)
+{
+    auto question = Question();
+    res.render!("index.dt", req, question);
 }
 
 shared static this()
 {
+    auto router = new URLRouter;
+    router.get("/", &index);
+    router.get("*", serveStaticFiles("public/"));
+    
     auto settings = new HTTPServerSettings;
     settings.bindAddresses = ["127.0.0.1"];
     settings.port = 8000;
     
-    listenHTTP(settings, &handleRequest);
+    listenHTTP(settings, router);
 }
